82ca68d06040dad9b2be040b47b16c470ba25588
Shiham123 <s36651786@gmail.com>
2023-11-24 23:11:33 +0600

    json web token and user added to database

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-// shihamAssignment11
-//shihamAssignment11
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster-assignment-11.m6efgmp.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster-assignment-11.m6efgmp.mongodb.net/?retryWrites=true&w=majority`;
+const uri = 'mongodb://localhost:27017';
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,6 +26,32 @@ const run = async () => {
     // await client.connect();
     const surveyDatabase = client.db('polling-survey');
     const userCollection = surveyDatabase.collection('user');
+
+    // Json web token
+
+    app.post('/jwt', async (request, response) => {
+      const user = request.body;
+      const token = jwt.sign(user, process.env.DB_SECRETE_KEY, {
+        expiresIn: '365d',
+      });
+      response.status(200).send(token);
+    });
+
+    // POST METHOD
+
+    app.post('/users', async (request, response) => {
+      const user = request.body;
+      const query = { email: user.email };
+
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser)
+        return response
+          .status(404)
+          .send({ message: 'user exits', inserted: null });
+
+      const result = await userCollection.insertOne(user);
+      response.status(200).send(result);
+    });
 
     await client.db('admin').command({ ping: 1 });
     console.log('You successfully connected to MongoDB!');
